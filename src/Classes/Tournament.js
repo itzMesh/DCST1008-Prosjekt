330e9dcb6286006cc425946a35bf1758949c14b0
Jo Remvik <jovre@stud.ntnu.no>
2022-03-22 13:50:34 +0100

    Moved functionality to Rounds

--- src/Classes/Tournament.js
+++ src/Classes/Tournament.js
@@ -1,8 +1,9 @@
-import ShadowTeam from './ShadowTeam';
+import Round from './Round';
 
 class Torunament {
 	generalSettings = null;
 	teams = [];
+	rounds = [];
 
 	constructor() {}
 
@@ -15,65 +16,20 @@ class Torunament {
 	}
 
 	createBrackets() {
-		this.rounds = this.setBracketRounds(this.teams);
-		this.teams = this.fillOutBracket(this.teams);
-		this.teams.sort((a, b) => {
-			if (a.averageTrophies < b.averageTrophies) {
-				return 1;
-			}
-			if (a.averageTrophies > b.averageTrophies) {
-				return -1;
-			}
-			return 0;
-		});
-		this.setseeds();
-		this.teams = this.sortTeams(this.teams);
+		this.numberOfRounds = this.setBracketRounds(this.teams);
 	}
 
 	setBracketRounds(teams) {
-		let rounds = 0;
+		let numberOfRounds = 0;
 		while (Math.pow(2, rounds) < teams.length) {
-			rounds += 1;
+			if (numberOfRounds == 0) {
+				this.rounds.push(new Round(0, teams));
+			} else {
+				this.rounds.push(new Round(numberOfRounds));
+			}
+			numberOfRounds += 1;
 		}
 		return rounds;
-	}
-
-	fillOutBracket(teams) {
-		while (teams.length < Math.pow(2, this.rounds)) {
-			teams.push(new ShadowTeam());
-		}
-		return teams;
-	}
-
-	setseeds() {
-		for (const i in this.teams) {
-			this.teams[i].seed = parseInt(i) + 1;
-		}
-	}
-
-	sortTeams(teams) {
-		let sortedarr = [];
-
-		let ink = 0;
-		let start = 0;
-		for (let i = 1; i <= teams.length; i = Math.pow(2, ink)) {
-			for (let j = start; j < Math.pow(2, ink); j++) {
-				start = j;
-				let numb = j;
-				if (sortedarr.length == 0) {
-					sortedarr.push(teams[0]);
-				} else {
-					for (let k = 0; k < sortedarr.length; k++) {
-						if (sortedarr[k].seed + teams[numb].seed == i + 1) {
-							sortedarr.splice(k + 1, 0, teams[numb]);
-							break;
-						}
-					}
-				}
-			}
-			ink++;
-		}
-		return sortedarr;
 	}
 }
 
