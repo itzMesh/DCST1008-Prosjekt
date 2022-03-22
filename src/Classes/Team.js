0cadea5e7ff02673d0351d1d1b4dc6ff4a67ce88
Jo Remvik <jovre@stud.ntnu.no>
2022-03-22 10:15:46 +0100

    Added constructors

--- src/Classes/Team.js
+++ src/Classes/Team.js
@@ -1,3 +1,17 @@
 import ShadowTeam from './ShadowTeam';
 
-class Team extends ShadowTeam {}
+class Team extends ShadowTeam {
+	constructor(member1,member2,name){
+		this.averageTrophies=(member1.trophies+member2.trophies)/2
+		this.teamMembers[0]=member1
+		this.teamMembers[1]=member2
+		this.teamName=name
+	}
+	constructor(member){
+		this.averageTrophies=member.trophies
+		this.teamMembers[0]=member
+		this.teamName=member.name
+	}
+}
+
+export default Team
