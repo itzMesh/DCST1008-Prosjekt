17a3ab1e22df7bfb8f2cbcf3206f1974cf3eadd9
martishv <martishv@stud.ntnu.no>
2022-03-23 15:35:35 +0100

    fikset slik at man ser alle tournamentsene og at når man trykker på disse går man til den tournamenten man vil til:)

--- src/overview.js
+++ src/overview.js
@@ -2,66 +2,83 @@ import * as React from 'react';
 import { Component } from 'react-simplified';
 import ReactDOM from 'react-dom';
 import { NavLink, HashRouter, Route } from 'react-router-dom';
-
+import { pool } from './mysql-pool';
 
 export class Choose extends Component {
-  render() {
-    return (
-        <div>
-          <br/> Overview of Tournaments <br/>
+	tournaments = [];
+	render() {
+		return (
+			<div>
+				<br /> Overview of Tournaments <br />
+				{this.tournaments.map((tournament) => (
+					<li key={tournament.TournamentID}>
+						<NavLink to={'/tournamentpage/' + tournament.TournamentID}>
+							{tournament.TournamentName}
+						</NavLink>
+					</li>
+				))}
+				<NavLink to="/new">New tournament</NavLink>
+			</div>
+		);
+	}
+	mounted() {
+		pool.query('SELECT * FROM Tournament', (error, results) => {
+			if (error) return console.error(error); // If error, show error in console (in red text) and return
 
-          <NavLink to="/tournamentPage">Tournament 1<br/><br/></NavLink>
-
-
-          <NavLink to="/new">New tournament</NavLink>
-        </div>
-    );
-  }
+			this.tournaments = results;
+		});
+	}
 }
 
 export class New extends Component {
-
-
-  render() {
-    return <div>
-
-      <br/>Select tournament type 
-
-        <select value={this.name} onChange={(event) => (this.name = event.currentTarget.value)}>
-          <option value="bracket">Bracket</option>
-          <option value="roundrobin">Round robin</option>
-        </select><br/><br/>
-
-        Select match type 
-
-        <select value={this.name} onChange={(event) => (this.name = event.currentTarget.value)}>
-          <option value="">1v1</option>
-          <option value="">2v2 - Generated teams</option>
-          <option value="">2v2 - Custom Teams</option>
-          <option value="">1v1 Double Elixir</option>
-          <option value="">2v2 - Double Elixir</option>
-          <option value="">2v2 - Double Elixir</option>
-        </select><br/><br/>
-
-        Amount of rounds
-
-        <select value={this.name} onChange={(event) => (this.name = event.currentTarget.value)}>
-          <option value="bracket">Best of 1</option>
-          <option value="roundrobin">Best of 3</option>
-        </select><br/><br/>
-
-        <NavLink to="/bracket">
-          <button type="button">Create tournament</button>
-        </NavLink>
-
-        
-      
-    </div>;
-  }
+	render() {
+		return (
+			<div>
+				<br />
+				Select tournament type
+				<select
+					value={this.name}
+					onChange={(event) => (this.name = event.currentTarget.value)}
+				>
+					<option value="bracket">Bracket</option>
+					<option value="roundrobin">Round robin</option>
+				</select>
+				<br />
+				<br />
+				Select match type
+				<select
+					value={this.name}
+					onChange={(event) => (this.name = event.currentTarget.value)}
+				>
+					<option value="">1v1</option>
+					<option value="">2v2 - Generated teams</option>
+					<option value="">2v2 - Custom Teams</option>
+					<option value="">1v1 Double Elixir</option>
+					<option value="">2v2 - Double Elixir</option>
+					<option value="">2v2 - Double Elixir</option>
+				</select>
+				<br />
+				<br />
+				Amount of rounds
+				<select
+					value={this.name}
+					onChange={(event) => (this.name = event.currentTarget.value)}
+				>
+					<option value="bracket">Best of 1</option>
+					<option value="roundrobin">Best of 3</option>
+				</select>
+				<br />
+				<br />
+				<NavLink to="/bracket">
+					<button type="button">Create tournament</button>
+				</NavLink>
+			</div>
+		);
+	}
 }
 
 export class Overview extends Component {
-  render() {
-    return <div>Overview</div>;
-  }
+	render() {
+		return <div>Overview</div>;
+	}
 }
