5e9c816196fd687cfad9ece346ae195816821ab4
Nicolai Forsberg Sommerfelt <nicolafs@stud.ntnu.no>
2022-03-24 14:33:03 +0100

    removing teams

--- src/players.js
+++ src/players.js
@@ -3,65 +3,110 @@ import { Component } from 'react-simplified';
 import ReactDOM from 'react-dom';
 import { NavLink, HashRouter, Route } from 'react-router-dom';
 
-
 export class Add extends Component {
-  name = '';
-  trophies = '';
-  team = '';
-  form = null;
+	team = '';
+	name1 = '';
+	trophies1 = '';
+	name2 = '';
+	trophies2 = '';
+	teams = [];
+	form = null;
 
-  render() {
-    return (
-      <form ref={(instance) => (this.form = instance)}>
-        <br /><input
-          type="text"
-          value={this.team}
-          placeholder="Team name"
-          size = '25x'
-          onChange={(event) => (this.team = event.currentTarget.value)}
-          required
-        />
-        <br /><input
-          type="text"
-          value={this.name}
-          placeholder="Nickname"
-          size = '10'
-          onChange={(event) => (this.name = event.currentTarget.value)}
-          required
-        />
-        <input
-          type="text"
-          value={this.trophies}
-          placeholder="Trophies"
-          size = '10'
-          onChange={(event) => (this.trophies = event.currentTarget.value)}
-          required
-        />
+	render() {
+		return (
+			<div>
+				<form ref={(instance) => (this.form = instance)}>
+					<br />
+					<input
+						type="text"
+						value={this.team}
+						placeholder="Team name"
+						size="25x"
+						onChange={(event) => (this.team = event.currentTarget.value)}
+						required
+					/>
+					<br /> Player 1
+					<input
+						type="text"
+						value={this.name1}
+						placeholder="Nickname"
+						size="10"
+						onChange={(event) => (this.name1 = event.currentTarget.value)}
+						required
+					/>
+					<input
+						type="number"
+						value={this.trophies1}
+						placeholder="Trophies"
+						size="10"
+						onChange={(event) => (this.trophies1 = event.currentTarget.value)}
+						required
+					/>
+					<br />
+					Player 2
+					<input
+						type="text"
+						value={this.name2}
+						placeholder="Nickname"
+						size="10"
+						onChange={(event) => (this.name2 = event.currentTarget.value)}
+						required
+					/>
+					<input
+						type="number"
+						value={this.trophies2}
+						placeholder="Trophies"
+						size="10"
+						onChange={(event) => (this.trophies2 = event.currentTarget.value)}
+						required
+					/>
+					<br />
+					<br />
+					<button type="button" onClick={this.buttonClicked}>
+						Add team
+					</button>
+				</form>
 
-        <br/><br/><button type="button" onClick={this.buttonClicked}>
-          Add team
-        </button>
-      </form>
-    );
-  }
+				<br />
+				<div>
+					{this.teams.map((team, i) => (
+						<div key={i}>
+							<div key={0}>Team: {team[0]}</div>
+							<div key={1}>
+								Name: {team[1][0]} Trophies {team[1][1]}
+							</div>
+							<div key={2}>
+								Name: {team[2][0]} Trophies {team[2][1]}
+							</div>
+							<button type="button" onClick={(i) => this.teams.splice(i, 1)}>
+								x
+							</button>
+							<br />
+							<br />
+						</div>
+					))}
+				</div>
+			</div>
+		);
+	}
 
-  buttonClicked() {
-    if (!this.form.reportValidity()) return;
+	buttonClicked() {
+		if (!this.form.reportValidity()) return;
 
-    let ny = React.createElement("div", {}, "Team: " + this.team + ' Name: ' + this.name + ' Trophies ' + this.trophies)
-    
-    let nytt = document.createElement("div")
-    nytt.id = 'nytt';
-    document.body.appendChild(nytt);
+		this.teams.push([this.team, [this.name1, this.trophies1], [this.name2, this.trophies2]]);
 
-    ReactDOM.render(
-      ny,
-      document.getElementById('nytt')
-    );
+		console.log(this.teams);
 
-  }
+		// let ny = React.createElement("div", {}, "Team: " + this.team + ' Name: ' + this.name1 + ' Trophies: ' + this.trophies1 +
+		//                                         ' Name: ' + this.name2 + ' Trophies: ' + this.trophies2)
+
+		// let nytt = document.createElement("div")
+		// nytt.id = 'nytt';
+		// document.body.appendChild(nytt);
+
+		// ReactDOM.render(
+		//   ny,
+		//   document.getElementById('nytt')
+		// );
+	}
 }
-
-
-
-
