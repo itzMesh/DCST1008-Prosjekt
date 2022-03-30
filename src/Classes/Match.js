711c460659e7c87e43f217f71a34b7d5d62567ab
martishv <martishv@stud.ntnu.no>
2022-03-30 13:31:06 +0200

    fjernet Completed fra GameMatch

--- src/Classes/Match.js
+++ src/Classes/Match.js
@@ -6,16 +6,14 @@ class Match {
 	matchSeed;
 	winner;
 	matchFinished;
-	completed;
 
-	constructor(team0, team1, matchNumber, round, ind, score0, score1, completed) {
+	constructor(team0, team1, matchNumber, round, ind, score0, score1) {
 		this.round = round;
 		this.ind = ind;
 		this.teams[0] = team0;
 		this.teams[1] = team1;
 		this.matchNumber = matchNumber;
 		this.matchSeed = team0.seed < team1.seed ? team0.seed : team1.seed;
-		this.completed = completed;
 		if (score0 != false && score1 != false) {
 			this.updateScore(score0, score1);
 		}
