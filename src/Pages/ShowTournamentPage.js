a7497c97ff76f24175d59b9bbda0514e2443bbc4
martishv <martishv@stud.ntnu.no>
2022-04-05 16:18:54 +0200

    brakets blir faen meg laget!!!!!!!!!

--- src/Pages/ShowTournamentPage.js
+++ src/Pages/ShowTournamentPage.js
@@ -11,6 +11,8 @@ import { run } from '@babel/core/lib/transformation';
 let tournamentID = 0;
 let hoyde = [];
 export class ShowTournamentPage extends Component {
+	loaded = false;
+	alreadyLoaded = false;
 	tournamentp = tournamentplayer[1] > tournamentplayers[1] ? tournamentplayer : tournamentplayers;
 	tournamentObject =
 		this.tournamentp[1] > tournamentPageObj[1] ? this.tournamentp[0] : tournamentPageObj[0];
@@ -25,22 +27,22 @@ export class ShowTournamentPage extends Component {
 					height={this.length * 150 + 186}
 					width={this.tournamentObject.numberOfRounds * 500}
 					style={{ zIndex: '-1', position: 'absolute' }}
-				></canvas>
+				>
+					{(this.loaded = true)}
+				</canvas>
 
-				<p>{this.tournamentObject.name}</p>
-				<p>{this.tournamentObject.generalSettings.gamemode}</p>
+				<div className="infon">{this.tournamentObject.name}</div>
+				<div className="infot">{this.tournamentObject.generalSettings.gamemode}</div>
+
 				<div>
 					<br />
 					<em className="login" onClick={this.save} type="button">
 						Save {this.brackets(this.tournamentObject.numberOfRounds)}
 					</em>
-					<em className="login" onClick={this.tegn} type="button">
-						tegn
-					</em>
 				</div>
 				<div className="grid" id="grid">
 					{this.tournamentObject.rounds.map((round) => (
-						<div>
+						<div onLoad={() => this.tegn(round.matches.length / 2)}>
 							<div
 								key={round.roundNumber}
 								id={round.roundNumber}
@@ -50,7 +52,8 @@ export class ShowTournamentPage extends Component {
 							>
 								Round {round.roundNumber + 1}:
 								<div className="grid-cell" style={{ height: this.length * 150 }}>
-									{round.roundNumber == 0 ? (
+									{round.roundNumber == 0 ||
+									this.tournamentObject.generalSettings.type != 'bracket' ? (
 										<em></em>
 									) : (
 										<div
@@ -129,11 +132,6 @@ export class ShowTournamentPage extends Component {
 													))}
 											</div>{' '}
 											<br />
-											{/* {round.roundNumber == 0 ? (
-												<em></em>
-											) : (
-												<div style={{ height: '70px' }}></div>
-											)} */}
 										</div>
 									))}
 								</div>
@@ -159,63 +157,28 @@ export class ShowTournamentPage extends Component {
 		);
 	}
 	tegn() {
-		let tileggY = 0;
-		console.log(this.length);
-		for (let i = 0; i < this.length / 2; i++) {
-			let c = document.getElementById('canvas');
-			var ctx = c.getContext('2d');
-			ctx.beginPath();
-			ctx.moveTo(410, 170 + tileggY);
-			ctx.lineTo(510, 170 + tileggY);
-			ctx.moveTo(410, 340 + tileggY);
-			ctx.lineTo(510, 340 + tileggY);
-			ctx.lineTo(510, 170 + tileggY);
-			ctx.moveTo(510, 255 + tileggY);
-			ctx.lineTo(580, 255 + tileggY);
-			ctx.lineWidth = 10;
-			ctx.stroke();
-			tileggY = tileggY + 280;
-			console.log();
-		}
-		tileggY = 0;
-		for (let i = 0; i < this.length / 4; i++) {
-			let c = document.getElementById('canvas');
-			var ctx = c.getContext('2d');
-			ctx.beginPath();
-			ctx.moveTo(910, 240 + tileggY);
-			ctx.lineTo(1010, 240 + tileggY);
-			ctx.moveTo(910, 560 + tileggY);
-			ctx.lineTo(1010, 560 + tileggY);
-			ctx.lineTo(1010, 240 + tileggY);
-			ctx.moveTo(1010, 400 + tileggY);
-			ctx.lineTo(1110, 400 + tileggY);
-
-			ctx.lineWidth = 10;
-			ctx.stroke();
-			tileggY = tileggY + 560;
-			console.log();
-		}
-		tileggY = 0;
-		for (let i = 0; i < this.length / 8; i++) {
-			let c = document.getElementById('canvas');
-			var ctx = c.getContext('2d');
-			ctx.beginPath();
-			ctx.moveTo(1410, 400 + tileggY);
-			ctx.lineTo(1510, 400 + tileggY);
-			ctx.moveTo(1410, 1000 + tileggY);
-			ctx.lineTo(1510, 1000 + tileggY);
-			ctx.lineTo(1510, 400 + tileggY);
-			ctx.moveTo(1510, 670 + tileggY);
-			ctx.lineTo(1610, 670 + tileggY);
-
-			// ctx.lineTo(1510, 560 + tileggY);
-			// ctx.lineTo(1510, 400 + tileggY);
-			// ctx.moveTo(1510, 400 + tileggY);
-			// ctx.lineTo(1610, 400 + tileggY);
-
-			ctx.lineWidth = 10;
-			ctx.stroke();
-			console.log();
+		let startX = 410;
+		let startY = 170;
+		console.log(this.tournamentObject.rounds);
+		for (let j = 0; j < this.tournamentObject.rounds.length - 1; j++) {
+			let drawX = startX + j * 540;
+			let drawY = startY + hoyde[j];
+			let tileggY = 0;
+			for (let i = 0; i < this.length / 2 ** (j + 1); i++) {
+				let c = document.getElementById('canvas');
+				var ctx = c.getContext('2d');
+				ctx.beginPath();
+				ctx.moveTo(drawX, drawY + tileggY);
+				ctx.lineTo(drawX + 100, drawY + tileggY);
+				ctx.moveTo(drawX, drawY + 140 * 2 ** j + tileggY);
+				ctx.lineTo(drawX + 100, drawY + 140 * 2 ** j + tileggY);
+				ctx.lineTo(drawX + 100, drawY + tileggY);
+				ctx.moveTo(drawX + 100, drawY + 70 * 2 ** j + tileggY);
+				ctx.lineTo(drawX + 170, drawY + 70 * 2 ** j + tileggY);
+				ctx.lineWidth = 10;
+				ctx.stroke();
+				tileggY = tileggY + 280 * 2 ** j;
+			}
 		}
 	}
 	brackets(e) {
@@ -331,5 +294,17 @@ export class ShowTournamentPage extends Component {
 			let message = await kjør([this.tournamentObject]);
 			console.log(message);
 		})();
+	}
+	mounted() {
+		setInterval(() => {
+			if (
+				this.loaded &&
+				!this.alreadyLoaded &&
+				this.tournamentObject.generalSettings.type == 'bracket'
+			) {
+				this.tegn();
+				this.alreadyLoaded = true;
+			}
+		}, 100);
 	}
 }
