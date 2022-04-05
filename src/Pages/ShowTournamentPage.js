2c2cb8282dbc4567cffaac464b4803e8a39f7e8d
martishv <martishv@stud.ntnu.no>
2022-04-05 11:11:55 +0200

    bracket som er fine, men round robin er facked

--- src/Pages/ShowTournamentPage.js
+++ src/Pages/ShowTournamentPage.js
@@ -9,12 +9,12 @@ import { template } from '@babel/core';
 import { run } from '@babel/core/lib/transformation';
 
 let tournamentID = 0;
-
+let hoyde = [];
 export class ShowTournamentPage extends Component {
 	tournamentp = tournamentplayer[1] > tournamentplayers[1] ? tournamentplayer : tournamentplayers;
 	tournamentObject =
 		this.tournamentp[1] > tournamentPageObj[1] ? this.tournamentp[0] : tournamentPageObj[0];
-
+	length = this.tournamentObject.rounds[0].matches.length;
 	render() {
 		if (!this.tournamentObject) return null;
 
@@ -22,6 +22,13 @@ export class ShowTournamentPage extends Component {
 		console.log(this.tournamentObject.TorunamentId, 'se her');
 		return (
 			<div className="small">
+				<canvas
+					id="canvas"
+					height={this.length * 150 + 186}
+					width={document.documentElement.clientWidth}
+					style={{ zIndex: '-1', position: 'absolute' }}
+				></canvas>
+
 				<p>{this.tournamentObject.name}</p>
 				<p>{this.tournamentObject.generalSettings.gamemode}</p>
 				<div>
@@ -29,8 +36,11 @@ export class ShowTournamentPage extends Component {
 					<em className="login" onClick={this.save} type="button">
 						Save
 					</em>
+					<em className="login" onClick={this.tegn} type="button">
+						tegn {this.test(this.tournamentObject.numberOfRounds)}
+					</em>
 				</div>
-				<div className="Grid">
+				<div className="Grid" id="grid">
 					{this.tournamentObject.rounds.map((round) => (
 						<div>
 							<div
@@ -41,80 +51,85 @@ export class ShowTournamentPage extends Component {
 								// }
 							>
 								Round {round.roundNumber + 1}:
-								<div className="Grid-cell">
-									<ul key={round.roundNumber + 'm'}>
-										{round.matches.map((match) => (
-											<div>
-												{round.roundNumber == 0 ? (
-													<div style={{ height: '20px' }}></div>
-												) : (
-													<div style={{ height: '80px' }}></div>
-												)}
-												<div key={match.matchNumber}>
-													<div key={0} style={{ fontSize: '25px' }}>
-														<NavLink
-															className="login"
-															to={
-																'/matches/edit/' +
-																round.roundNumber +
-																',' +
-																match.ind
-															}
-														>
-															Match {match.matchNumber}
-														</NavLink>
-													</div>
-													{match.teams
-														.filter(
-															(team) =>
-																team.constructor.name !=
-																'ShadowTeam'
-														)
-														.map((team, i) => (
-															<div key={team.id}>
-																<em key={0}>
-																	<b
-																		style={{
-																			color: 'white',
-																		}}
-																	>
-																		{match.results[i]}{' '}
-																	</b>
-																	<b>{team.name}</b>
-																	{team.teamMembers.length ==
-																	1 ? (
-																		<em></em>
-																	) : (
-																		<em>
-																			<b>:</b>
-																			{team.teamMembers.map(
-																				(member) => (
-																					<em
-																						key={
-																							member.name
-																						}
-																					>
-																						{'"' +
-																							member.name +
-																							'" '}
-																					</em>
-																				)
-																			)}
-																		</em>
-																	)}
-																</em>
-															</div>
-														))}
-												</div>{' '}
-												<br />
-												{round.roundNumber == 0 ? (
-													<div style={{ height: '10px' }}></div>
-												) : (
-													<div style={{ height: '70px' }}></div>
-												)}
-											</div>
-										))}
-									</ul>
+								<div className="Grid-cell" style={{ height: this.length * 150 }}>
+									{round.roundNumber == 0 ? (
+										<em></em>
+									) : (
+										<div
+											style={{
+												height: hoyde[round.roundNumber] + 'px',
+											}}
+										></div>
+									)}
+									{round.matches.map((match) => (
+										<div
+											style={{
+												height: 140 * 2 ** round.roundNumber + 'px',
+												width: '400px',
+											}}
+										>
+											<div key={match.matchNumber}>
+												<div key={0} style={{ fontSize: '25px' }}>
+													<NavLink
+														className="login"
+														to={
+															'/matches/edit/' +
+															round.roundNumber +
+															',' +
+															match.ind
+														}
+													>
+														Match {match.matchNumber}
+													</NavLink>
+												</div>
+												{match.teams
+													.filter(
+														(team) =>
+															team.constructor.name != 'ShadowTeam'
+													)
+													.map((team, i) => (
+														<div key={team.id}>
+															<em key={0}>
+																<b
+																	style={{
+																		color: 'white',
+																	}}
+																>
+																	{match.results[i]}{' '}
+																</b>
+																<b>{team.name}</b>
+																{team.teamMembers.length == 1 ? (
+																	<em></em>
+																) : (
+																	<em>
+																		<b>:</b>
+																		{team.teamMembers.map(
+																			(member) => (
+																				<em
+																					key={
+																						member.name
+																					}
+																				>
+																					{'"' +
+																						member.name +
+																						'" '}
+																				</em>
+																			)
+																		)}
+																	</em>
+																)}
+															</em>
+														</div>
+													))}
+											</div>{' '}
+											<br />
+											{/* {round.roundNumber == 0 ? (
+												<em></em>
+											) : (
+												<div style={{ height: '70px' }}></div>
+											)} */}
+										</div>
+									))}
 								</div>
 							</div>
 						</div>
@@ -137,7 +152,37 @@ export class ShowTournamentPage extends Component {
 			</div>
 		);
 	}
+	tegn() {
+		let tileggY = 0;
+		console.log(this.length);
+		for (let i = 0; i < this.length / 2; i++) {
+			let c = document.getElementById('canvas');
+			var ctx = c.getContext('2d');
+			ctx.beginPath();
+			ctx.moveTo(410, 170 + tileggY);
+			ctx.lineTo(510, 170 + tileggY);
+			ctx.moveTo(410, 340 + tileggY);
+			ctx.lineTo(510, 340 + tileggY);
+			ctx.lineTo(510, 170 + tileggY);
+			ctx.moveTo(510, 255 + tileggY);
+			ctx.lineTo(580, 255 + tileggY);
+			ctx.lineWidth = 10;
+			ctx.stroke();
+			tileggY = tileggY + 280;
+			console.log();
+		}
+	}
+	test(e) {
+		let out = 0;
+		hoyde = [0];
+		for (let i = 0; i < e; i++) {
+			out += 70 * Math.pow(2, i);
+			hoyde.push(out);
+		}
+		console.log(hoyde);
 
+		return out;
+	}
 	save() {
 		function delTournament(inn) {
 			console.log(inn, '1');
